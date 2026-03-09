1a12d264ba8515d3f00d62270d09557f2e04e2ed
Edu-oliveira7 <aksedu0@gmail.com>
2026-03-09 15:14:23 -0300

    Inicio da configuraçao do models do backend.

--- backend/apps/workouts/models.py
+++ backend/apps/workouts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Workout(models.Model):
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="workouts"
+    )
+
+    name = models.CharField(max_length=100)
+    day = models.CharField(max_length=20)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+class Exercise(models.Model):
+    workout = models.ForeignKey(
+        Workout,
+        on_delete=models.CASCADE,
+        related_name="exercises"
+    )
+    name = models.CharField(max_length=100)
+    sets = models.IntegerField()
+    reps = models.IntegerField()
+    weight = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
